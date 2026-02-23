ceda480801b514b527955ace913f5ab322f5ffab
marcocentofanti <marcocentofanti@hotmail.it>
2026-02-23 18:44:08 +0100

    add new article

--- src/assets/ArticleList.jsx
+++ src/assets/ArticleList.jsx
@@ -2,12 +2,33 @@ import { useState } from "react"
 
 export default function ArticleList() {
   const [articles, SetArticles] = useState(["Guerra e pace", "Il Processo", "Il barone rampante", "Moby Dick", ])
+  const [newArticle, SetNewArticle] = useState(``)
+
+  const addArtilce = (e) => {
+    e.preventDefault()
+    SetArticles([...articles, newArticle])
+    SetNewArticle("")
+
+  }
 
   return (
-    <ul>
-      {articles.map((singlearticle, index) => (
-        <li key={index}>{singlearticle}</li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {articles.map((singlearticle, index) => (
+          <li key={index}>{singlearticle}</li>
+        ))}
+      </ul>
+      <form onSubmit={addArtilce}>
+        <input 
+          type="text"
+          value={newArticle}
+          onChange={(e) => SetNewArticle(e.target.value)}/>
+          <button
+            type="submit">
+              Aggiungi Articolo
+            </button>
+          
+      </form>
+    </>
     )
   }
